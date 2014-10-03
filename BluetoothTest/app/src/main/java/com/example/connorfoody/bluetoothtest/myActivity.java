52086a1689551c489f01129169c87b07db329ec9
ConnorFoody <connorfoody@gmail.com>
2014-10-03 16:08:08 -0400

    updated bluetooth tests
    based on online stuff

--- BluetoothTest/app/src/main/java/com/example/connorfoody/bluetoothtest/myActivity.java
+++ BluetoothTest/app/src/main/java/com/example/connorfoody/bluetoothtest/myActivity.java
@@ -1,53 +1,43 @@
-package com.example.connorfoody.sevenminutesoftaha;
+package com.example.connorfoody.bluetoothtest;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.os.Handler;
 import android.os.Message;
+import android.view.Menu;
+import android.view.MenuItem;
+import java.util.UUID;
 import android.widget.TextView;
 
 
-
-public class MyActivity extends Activity {
-    SoundReader reader = null;
-    public Handler mhandle = new Handler(){
-        @Override
-        public void handleMessage(Message msg){
-
-            if(msg.what == 1) {
-                TextView text = (TextView) findViewById(R.id.output);
-                text.setText("GOOD: " + msg.obj);
-            }
-            else if(msg.what == -1){
-                TextView text = (TextView) findViewById(R.id.output);
-                text.setText("ERROR: " + msg.obj);
-
-
-            }
-            else{
-                super.handleMessage(msg);
-
-            }
-        }
-    };
+public class myActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        //TextView text = (TextView) findViewById(R.id.output);
 
+        // start a new thread to connect and read the serial
+        BluetoothReader m_reader = new BluetoothReader(handler,"20:14:04:18:23:59", UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+        m_reader.start();
 
-        reader = new SoundReader(mhandle);
-        //mhandle.postDelayed(reader, 3000);
-        //mhandle.post(reader);
-        reader.start();
-        //Thread th = new Thread(reader);
-        //th.start();
     }
 
+    public Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            TextView text = (TextView) findViewById(R.id.output);
+            if(msg.what == 1){
+                text.append( "\n" + msg.obj);
+            }
+            else if(msg.what == -1){
+                text.append("\nERROR: " + msg.obj + "\n");
+            }
+            else{
+                super.handleMessage(msg);
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
